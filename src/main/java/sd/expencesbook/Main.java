2235e0ae9daea3798bf42c821490d641bce0de02
gitstanhub <stasdmitruk1@gmail.com>
2023-02-06 04:21:34 +0100

    Updated the lanch method name

--- src/main/java/sd/expencesbook/Main.java
+++ src/main/java/sd/expencesbook/Main.java
@@ -1,7 +1,9 @@
 package sd.expencesbook;
 
+import sd.expencesbook.services.Application;
+
 public class Main {
     public static void main(String[] args) {
-
+        new Application().launch();
     }
 }
