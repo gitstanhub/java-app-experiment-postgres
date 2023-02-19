054bb6779026e1da63141df9d2cc5b030e7a551b
gitstanhub <stasdmitruk1@gmail.com>
2023-02-19 05:27:31 +0100

    Added PostGresExpenseRepository implementation draft

--- src/main/java/sd/expensestracker/db/ExpenseRepository.java
+++ src/main/java/sd/expensestracker/db/ExpenseRepository.java
@@ -1,10 +1,11 @@
 package sd.expensestracker.db;
 
+import sd.expensestracker.entities.AccountEntity;
 import sd.expensestracker.entities.ExpenseEntity;
 
 import java.util.List;
 
 public interface ExpenseRepository {
 
-    List<ExpenseEntity> getAll();
+    List<ExpenseEntity> getAllByAccount(AccountEntity entity);
 }
