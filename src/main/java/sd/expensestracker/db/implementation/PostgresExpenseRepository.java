7869298995db2ebdfd0a802a4fd1a802cc01c9e5
Stanislav Dmitruk <stanislavdmitruk@stanislavs-mbp.speedport.ip>
2023-02-20 01:42:11 +0100

    Added ExpenseEntityRowMapper class and included new Expense Description column

--- src/main/java/sd/expensestracker/db/implementation/PostgresExpenseRepository.java
+++ src/main/java/sd/expensestracker/db/implementation/PostgresExpenseRepository.java
@@ -1,5 +1,8 @@
 package sd.expensestracker.db.implementation;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import sd.expensestracker.db.DataSourceProvider;
+import sd.expensestracker.db.ExpenseEntityRowMapper;
 import sd.expensestracker.db.ExpenseRepository;
 import sd.expensestracker.entities.AccountEntity;
 import sd.expensestracker.entities.ExpenseEntity;
@@ -8,7 +11,15 @@ import java.util.List;
 
 public class PostgresExpenseRepository implements ExpenseRepository {
 
-    public List<ExpenseEntity> getAllByAccount(AccountEntity entity) {
-        return null;
+    public JdbcTemplate template = new JdbcTemplate(DataSourceProvider.getDataSource());
+
+    @Override
+    public List<ExpenseEntity> getAllByAccount(AccountEntity account) {
+        return template.query("SELECT * FROM expense", new ExpenseEntityRowMapper());
+    }
+
+    @Override
+    public void addExpense(AccountEntity account) {
+
     }
 }
